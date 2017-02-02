4fb9da209d815dc94e050abfb5872503ed7266a4
nialloc9@gmail.com <nialloc9@gmail.com>
2017-02-02 02:36:28 +0000

    Cleaned up and added package.json. Next: Add Screenshots

---
+++ tests/test.js
@@ -0,0 +1,9 @@
+describe("true", function(){
+    it("Should be true", function(){
+        expect(true).toBeTruthy();
+    });
+});
+
+/*
+Even though not in the brief. I would use jasmine to test the application.
+ */
